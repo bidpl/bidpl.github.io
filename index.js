d0a6e98fc927ab49d5f0a6f459b22f3117db2ef0
bidpl <binhminh292@gmail.com>
2018-02-28 21:56:03 -0800

    Modified index.html and index.js to work like desmos graph

--- index.js
+++ index.js
@@ -4,11 +4,35 @@ var total;
 
 var p;
 var q;
+var total;
 
 function update(){
-  dominant = document.getElementById("dominantInput").value;
-  recessive = document.getElementById("recessiveInput").value;
+  dominant = parseInt(document.getElementById("dominantInput").value);
+  recessive = parseInt(document.getElementById("recessiveInput").value);
   
-  console.log(dominant);
+  total = dominant + recessive;
+  
+  q = Math.sqrt(recessive/total);
+  p = 1-q;
+  
+  setText("total_output", total);
+  
+  setText("p_output", p);
+  setText("q_output", q);
+  
+  setText("p^2_output", Math.pow(p, 2));
+  setText("2pq_output", 2*p*q);
+  setText("q^2_output", Math.pow(q, 2));
+  
+  setText("homoDominant_output", Math.pow(p,2)*total);
+  setText("homoDominant-Rounded_output", Math.round(Math.pow(p,2)*total));
+  
+  setText("hetero_output", 2*p*q*total);
+  setText("hetero-Rounded_output", Math.round(2*p*q*total));
+  
+  setText("homoRecessive_output", recessive);
 }
 
+function setText(id, text){
+  document.getElementById(id).innerHTML = text;
+}
