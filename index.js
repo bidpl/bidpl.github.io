a58e682abb9af232c0a56bee6ecdc687731bd8f2
bidpl <binhminh292@gmail.com>
2018-02-28 20:26:53 -0800

    Added index.html, index.js, and index.css

---
+++ index.js
@@ -0,0 +1,14 @@
+var dominant;
+var recessive;
+var total;
+
+var p;
+var q;
+
+function update(){
+  dominant = document.getElementById("dominantInput").value;
+  recessive = document.getElementById("recessiveInput").value;
+  
+  console.log(dominant);
+}
+
